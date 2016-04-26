5600693dc87a5dd5bcc11902b5f2634723bfdb58
Pakpon <pakpon.j@ku.th>
2016-04-26 17:16:41 +0700

    searchResult can now show data

--- backend/views/tableApp.js
+++ backend/views/tableApp.js
@@ -1,13 +1,16 @@
 var app = angular.module('tableApps', []);
 
+console.log('tableApp')
+
 app.controller('tableCtrl', function ($scope, $http) {
+    console.log("hello");
     $http.get("/api/upload/subjects")
         .then(function(response) {
             $scope.status = response.status;
             $scope.data = response.data;
+            console.log(data);
         }, function(response) {
             $scope.data = response.data || "Request failed";
             $scope.status = response.status;
-        })
-    })
+    });
 });
