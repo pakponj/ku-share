88b57d89a7abcf3bedc24de0ab87c5c0b581fd5c
Pakpon <pakpon.j@ku.th>
2016-04-26 13:36:32 +0700

    add dataService script for requesting data from backend

---
+++ backend/views/scripts/dataService.js
@@ -0,0 +1,14 @@
+﻿angular.module('serviceApp',[])
+.controller('userServiceCtrl', function ($scope, $http) {
+    $http.get('/api/category').success(function (data) {
+        $scope.username = data;
+        alert('Load username successed');
+    })
+});
+angular.module('serviceApp', [])
+.controller('getSubjectsCtrl', ['$scope', function ($http) {
+    $http.get('/api/upload/subjects').success(function (data) {
+        $scope.subjects = data;
+        alert('Load subjects successed');
+    })
+}]);
