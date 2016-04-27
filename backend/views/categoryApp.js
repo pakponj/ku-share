7e89b45e90cf5751e52074c8f8d06a7ff820ebf7
Pipatpol <pipatpol.t@ku.th>
2016-04-27 18:59:21 +0700

    provide view api

--- backend/views/categoryApp.js
+++ backend/views/categoryApp.js
@@ -3,20 +3,6 @@ var app = angular.module('catApps', ['ngRoute'])
         // configure the routing rules here
         $routeProvider.when('/category/:categoryName', {
             controller: 'subjectCtrl',
-            //controllerAs: 'app',
-            //"resolve": {
-            //    'data': [
-            //        '$http',
-            //        function ($http) {
-            //            return $http
-            //                .get('api/show/' + $routeProvider.$routeParams.categoryName + '/subjects')
-            //                    .then(
-            //                        function success(response) { console.log('Success'); },
-            //                        function error(reason) { console.log('failed'); }
-            //                    );
-            //        }
-            //    ]
-            //}
         });
 
         // enable HTML5mode to disable hashbang urls
@@ -28,13 +14,13 @@ var app = angular.module('catApps', ['ngRoute'])
     .controller('subjectCtrl',['$scope', '$http', '$routeParams', '$location',function ($scope, $http, $routeParams, $location, $route) {
         console.log($location.path());
         var subject = ($location.path()).substring($location.path().indexOf('/',1)+1);
-        console.log(subject);   
+        console.log(subject);
         //console.log($routeParams.categoryName);
         //console.log($route.current.params);
         //console.log(JSON.stringify($routeParams));
         $http.get("/api/show/"+(subject)+"/subjects")
             .then(function(response) {
-                $scope.category = $routeParams.categoryName;
+                $scope.category = subject;
                 $scope.status = response.status;
                 $scope.data = response.data;
                 console.log($scope.data);
