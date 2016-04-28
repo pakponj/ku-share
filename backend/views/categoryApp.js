cf8813b2ddcb41c3fdf253315e099c6debf53fdb
Pakpon <pakpon.j@ku.th>
2016-04-28 18:07:43 +0700

    re-bind uploadFilepage dropdownCtrl

--- backend/views/categoryApp.js
+++ backend/views/categoryApp.js
@@ -1,11 +1,14 @@
-var app = angular.module('catApps', ['ngRoute'])
-    .config(['$routeProvider','$locationProvider',function ($routeProvider, $locationProvider) {
+var app = angular.module('catApps',['ngRoute', 'ngCookies'])
+    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
         // configure the routing rules here
         $routeProvider.when('/category/:categoryName', {
             controller: 'subjectCtrl'
         })
         .when('view/:openfile', {
             controller: 'fileOpenCtrl'
+        })
+        .when('/category', {
+            controller: 'tableCatCtrl'
         });
 
         // enable HTML5mode to disable hashbang urls
@@ -14,29 +17,26 @@ var app = angular.module('catApps', ['ngRoute'])
             requireBase: false
         });
     }])
-    .controller('subjectCtrl',['$scope', '$http', '$routeParams', '$location',function ($scope, $http, $routeParams, $location, $route) {
-        console.log($location.path());
+
+    .controller('subjectCtrl',['$scope', '$http', '$location',function ($scope, $http, $location) {
+        //console.log($location.path());
         var subject = ($location.path()).substring($location.path().indexOf('/',1)+1);
         console.log(subject);
-        //console.log($routeParams.categoryName);
-        //console.log($route.current.params);
-        //console.log(JSON.stringify($routeParams));
         $http.get("/api/show/"+(subject)+"/subjects")
-            .then(function(response) {
-                $scope.category = subject;
+            .then(function (response) {
                 $scope.status = response.status;
                 $scope.data = response.data;
-                console.log($scope.data);
-            }, function(response) {
+            }, function (response) {
                 $scope.data = response.data || "Request failed";
                 $scope.status = response.status;
-                console.log($scope.data);
             });
     }])
+
     .controller('fileOpenCtrl', ['$scope', '$http', '$routeParams', '$location', function ($scope, $http, $routeParams, $location) {
-        console.log($location.path());
-        var fileToOpen = ($location.path()).substring(location.path().indexOf('/', 1) + 1);
-        console.log(fileToOpen);
+        console.log($routeParams)
+        //console.log($location.path());
+        //var fileToOpen = ($location.path()).substring(location.path().indexOf('/', 1) + 1);
+        //console.log(fileToOpen);
 
         $http.get('/api/view/file/' + fileToOpen)
             .then(function (response) {
@@ -45,6 +45,51 @@ var app = angular.module('catApps', ['ngRoute'])
                 console.log($scope.data);
             });
     }])
+
     .controller('HeaderCtrl', ['$scope', function ($scope) {
         $scope.header = { name: 'header.html', url: 'header.html' };
-    }]);
+    }])
+
+    .controller('tableCatCtrl', function ($scope, $http) {
+        $http.get("/api/show/category")
+            .then(function (response) {
+                $scope.status = response.status;
+                $scope.data = response.data;
+            }, function (response) {
+                $scope.data = response.data || "Request failed";
+                $scope.status = response.status;
+            });
+    })
+
+    .controller('userCtrl', ['$scope', '$cookies', function ($scope, $cookies) {
+
+        var now = new Date(),
+            exp = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+
+        $scope.saveUsername = function (username) {
+            console.log(username);
+            $cookies.put('username', username, {
+                expires: exp
+            });
+        };
+
+        $scope.getUsername = function () {
+            console.log($cookies.get('username'));
+            return $cookies.get('username');
+        };
+
+    }])
+
+    .controller('dropdownCtrl', ['$scope','$http', function ($scope, $http) {
+        $http.get('/api/upload/show/subject')
+            .then(function (response) {
+                $scope.subjects = response.data;
+                $scope.status = response.status;
+            });
+    }])
+
+    .controller('searchSender', ['$scope', function ($scope) {
+        var $scope
+    }])
+
+    .controller('searchResultCtrl', ['$scope', ]);
--- backend/views/scripts/categoryApp.js
+++
@@ -1,7 +0,0 @@
-﻿var categoryApp = angular.module('categoryApp', []);
-
-categoryApp.controller('CategoryListCtrl', function ($scope) {
-    $scope.categories = [
-        '1', '2', '3', '4', '5'
-    ];
-});
